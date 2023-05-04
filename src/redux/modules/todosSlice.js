4f98e59f503122b431188cfa655b3c975e563ce9
shinheeje <dbfk2637@naver.com>
2023-05-04 12:48:10 +0900

    first commit

---
+++ src/redux/modules/todosSlice.js
@@ -0,0 +1,49 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+/**
+ * 2초 지연을 시키는 함수입니다 (비동기 작업).
+ */
+import { waitTwoSeconds } from '../../utils';
+
+export const __addToDo = createAsyncThunk(
+  '__addToDo',
+  async (payload, thunkAPI) => {
+    waitTwoSeconds().then(() => thunkAPI.dispatch(addTodo(payload)))
+  }
+);
+
+export const __deleteTodo = createAsyncThunk(
+  '__deleteToDo',
+  async (payload, thunkAPI) => {
+    waitTwoSeconds().then(() => thunkAPI.dispatch(deleteTodo(payload)))
+   }
+);
+
+const initialState = {
+  list: [],
+};
+
+const todosSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      return {
+        list: [
+          ...state.list,
+          action.payload,
+        ]
+      }
+    },
+    deleteTodo: (state, action) => {
+      return {
+        list: state.list.filter((e) => {
+          return e.id !== action.payload
+        })
+      }
+    },
+  },
+});
+
+export const { addTodo, deleteTodo } = todosSlice.actions;
+export default todosSlice.reducer;
